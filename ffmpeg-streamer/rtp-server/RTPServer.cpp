9d2369a943935b8e7b771d5f88a7f7e687ef2346
Maxim Sidorov <m.sidorov.vedapro.ru>
2023-12-15 13:26:39 +0300

    fixed problem with routers

--- ffmpeg-streamer/rtp-server/RTPServer.cpp
+++ ffmpeg-streamer/rtp-server/RTPServer.cpp
@@ -10,6 +10,15 @@ RTPServer::RTPServer(Address addr) :
     std::cout << "Server listening on: " << addr.host() << " port: " << addr.port() << std::endl;
 }
 
+void RTPServer::onRequest(const Http::Request &request, Http::ResponseWriter response) {
+    auto it = routerHandler.find(request.resource());
+    if (it != routerHandler.end()) {
+        it->second(request, &response);
+    } else {
+        response.send(Http::Code::Not_Found, "Not Found");
+    }
+}
+
 void RTPServer::init(size_t thr) {
     auto opts = Http::Endpoint::options()
             .threads(static_cast<int>(thr));
@@ -18,24 +27,22 @@ void RTPServer::init(size_t thr) {
 }
 
 void RTPServer::start() {
-    httpEndpoint->setHandler(router.handler());
+    httpEndpoint->setHandler(shared_from_this());
     httpEndpoint->serve();
 }
 
 void RTPServer::setupRoutes() {
-    using namespace Rest;
-
-    //Routes::Get(router, "/heartbeat", Routes::bind(&RTPServer::handleHeartBeat, this));
-    //Routes::Get(router, "/play", Routes::bind(&RTPServer::play, this));
-    Routes::Get(router, "/heartbeat", Routes::bind(&RTPServer::stop, this));
+    routerHandler["/heartbeat"] = [this](const Http::Request &request, Http::ResponseWriter *response) {
+        handleHeartBeat(request, response);
+    };
 }
 
-void RTPServer::handleHeartBeat(const Rest::Request &request, Http::ResponseWriter response) {
-        /*nlohmann::json j;
-        j["type"] = "heartbeat";
-        j["data"] = {{"message", "Start Jetson Nano Server"}};*/
+void RTPServer::handleHeartBeat(const Http::Request &request, Http::ResponseWriter *response) {
+    nlohmann::json j;
+    j["type"] = "heartbeat";
+    j["data"] = {{"message", "Start Jetson Nano Server"}};
 
-    response.send(Http::Code::Ok, "OK!");
+    response->send(Http::Code::Ok, j.dump(), MIME(Application, Json));
 }
 
 void RTPServer::play(const Rest::Request &request, Http::ResponseWriter response) {
--- ffmpeg-streamer/rtp-server/RTPServer.hpp
+++ ffmpeg-streamer/rtp-server/RTPServer.hpp
@@ -2,6 +2,7 @@
 #define STREAMER_RTPSERVER_HPP
 
 #include <mutex>
+#include <unordered_map>
 
 #include <pistache/endpoint.h>
 #include <pistache/http.h>
@@ -9,21 +10,26 @@
 
 using namespace Pistache;
 
-class RTPServer {
+class RTPServer : public Http::Handler, public std::enable_shared_from_this<RTPServer> {
+    using RouterCallback = std::function<void(const Http::Request &request, Http::ResponseWriter *response)>;
+
 public:
     explicit RTPServer(Address addr);
+
+    HTTP_PROTOTYPE(RTPServer)
+    void onRequest(const Http::Request &request, Http::ResponseWriter response) override;
 
     void init(size_t thr = 2);
     void start();
 
 private:
     void  setupRoutes();
-    void handleHeartBeat(const Rest::Request& request, Http::ResponseWriter response);
+    void handleHeartBeat(const Http::Request& request, Http::ResponseWriter *response);
     void play(const Rest::Request& request, Http::ResponseWriter response);
     void stop(const Rest::Request& request, Http::ResponseWriter response);
 
     std::shared_ptr<Http::Endpoint> httpEndpoint;
-    Rest::Router router;
+    std::unordered_map<std::string, RouterCallback> routerHandler;
 };
 
 
